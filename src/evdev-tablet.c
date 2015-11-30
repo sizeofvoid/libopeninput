5952aa6b501bf9b5c17e6eb0af4ccd367b458b2c
Peter Hutterer <peter.hutterer@who-t.net>
2015-11-30 13:47:08 +1000

    tablet: break up a condition to improve readability
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/evdev-tablet.c
+++ src/evdev-tablet.c
@@ -410,8 +410,12 @@ tablet_check_notify_axes(struct tablet_dispatch *tablet,
 		} else {
 			enum libinput_tablet_tool_tip_state tip_state;
 
-			tip_state = tablet_has_status(tablet, TABLET_TOOL_IN_CONTACT) ?
-				LIBINPUT_TABLET_TOOL_TIP_DOWN : LIBINPUT_TABLET_TOOL_TIP_UP;
+			if (tablet_has_status(tablet,
+					      TABLET_TOOL_IN_CONTACT))
+				tip_state = LIBINPUT_TABLET_TOOL_TIP_DOWN;
+			else
+				tip_state = LIBINPUT_TABLET_TOOL_TIP_UP;
+
 			tablet_notify_axis(base,
 					   time,
 					   tool,
