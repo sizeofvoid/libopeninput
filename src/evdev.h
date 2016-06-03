7cf5e5a4a93476fec6cf80af203cfbbdb5ccd659
Peter Hutterer <peter.hutterer@who-t.net>
2016-06-03 11:19:23 +1000

    touchpad: restore the hysteresis by default
    A large part of the bugs seen right now are related to touchpads jittering too
    much. Fixing them one by one is entertaining, but time consuming. Right now
    the number of touchpads that require a hysteresis seem to outnumber those that
    don't, so switch the approach around: leave the hysteresis in place but
    disable it for those touchpads that don't need it.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    (cherry picked from commit 27078b2667def4ecde1f47b8258d510a576c8bb1)
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/evdev.h
+++ src/evdev.h
@@ -114,7 +114,7 @@ enum evdev_device_model {
 	EVDEV_MODEL_CYBORG_RAT = (1 << 14),
 	EVDEV_MODEL_CYAPA = (1 << 15),
 	EVDEV_MODEL_LENOVO_T450_TOUCHPAD= (1 << 17),
-	EVDEV_MODEL_WOBBLY_TOUCHPAD = (1 << 18),
+	EVDEV_MODEL_PRECISE_TOUCHPAD = (1 << 18),
 };
 
 struct mt_slot {
