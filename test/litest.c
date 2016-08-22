9550cd47b21413374ea791d28e614b57b416aecd
Peter Hutterer <peter.hutterer@who-t.net>
2016-08-22 17:16:21 +1000

    test: add a Wacom HID 4800 test device
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Jason Gerecke <jason.gerecke@wacom.com>

--- test/litest.c
+++ test/litest.c
@@ -400,6 +400,7 @@ extern struct litest_test_device litest_wacom_cintiq_24hdt_pad_device;
 extern struct litest_test_device litest_wacom_cintiq_13hdt_finger_device;
 extern struct litest_test_device litest_wacom_cintiq_13hdt_pen_device;
 extern struct litest_test_device litest_wacom_cintiq_13hdt_pad_device;
+extern struct litest_test_device litest_wacom_hid4800_tablet_device;
 
 struct litest_test_device* devices[] = {
 	&litest_synaptics_clickpad_device,
@@ -455,6 +456,7 @@ struct litest_test_device* devices[] = {
 	&litest_wacom_cintiq_13hdt_finger_device,
 	&litest_wacom_cintiq_13hdt_pen_device,
 	&litest_wacom_cintiq_13hdt_pad_device,
+	&litest_wacom_hid4800_tablet_device,
 	NULL,
 };
 
