2ec596c26551b33315392b7e32bc278956935f40
Mihail Konev <k.mvc@ya.ru>
2017-01-26 14:00:21 +1000

    autogen: add default patch prefix
    Signed-off-by: Mihail Konev <k.mvc@ya.ru>

--- autogen.sh
+++ autogen.sh
@@ -6,4 +6,8 @@ test -n "$srcdir" || srcdir=.
   cd "$srcdir" &&
   autoreconf --force -v --install
 ) || exit
+
+git config --local --get format.subjectPrefix >/dev/null 2>&1 ||
+    git config --local format.subjectPrefix "PATCH libinput"
+
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
