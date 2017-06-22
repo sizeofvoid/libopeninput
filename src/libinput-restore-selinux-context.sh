ec27aa3a78c549a78f97bab180e65ddbc23822c6
Peter Hutterer <peter.hutterer@who-t.net>
2017-06-23 09:35:27 +1000

    Skip restorecon if it cannot be found
    https://bugs.freedesktop.org/show_bug.cgi?id=101557
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/libinput-restore-selinux-context.sh
+++ src/libinput-restore-selinux-context.sh
@@ -6,5 +6,7 @@
 libdir="$1"
 sofile=$(basename "$2")
 
-echo "Restoring SELinux context on $MESON_INSTALL_DESTDIR_PREFIX/$libdir/$sofile"
-restorecon "$MESON_INSTALL_DESTDIR_PREFIX/$libdir/$sofile"
+if command -v restorecon >/dev/null; then
+	echo "Restoring SELinux context on $MESON_INSTALL_DESTDIR_PREFIX/$libdir/$sofile"
+	restorecon "$MESON_INSTALL_DESTDIR_PREFIX/$libdir/$sofile"
+fi
