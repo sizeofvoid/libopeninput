whot/libinput

Add libinput_device_switch_has_switch()
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

test/test-switch.c
@@ -29,6 +29,16 @@
 #include "libinput-util.h"
 #include "litest.h"
 
+START_TEST(lid_switch_has)
+{
+	struct litest_device *sw = litest_current_device();
+
+	ck_assert_int_eq(libinput_device_switch_has_switch(sw->libinput_device,
+							   LIBINPUT_SWITCH_LID),
+			 1);
+}
+END_TEST
+
 START_TEST(lid_switch)
 {
 	struct litest_device *sw = litest_current_device();
@@ -549,6 +559,7 @@ END_TEST
 void
 litest_setup_tests_lid(void)
 {
+	litest_add("lid:switch", lid_switch_has, LITEST_SWITCH, LITEST_ANY);
 	litest_add("lid:switch", lid_switch, LITEST_SWITCH, LITEST_ANY);
 	litest_add("lid:switch", lid_switch_double, LITEST_SWITCH, LITEST_ANY);
 	litest_add("lid:switch", lid_switch_down_on_init, LITEST_SWITCH, LITEST_ANY);

sizeofvoid/libopeninput

test/test-switch.c
@@ -39,6 +39,16 @@ START_TEST(switch_has_cap)
 }
 END_TEST
 
+START_TEST(switch_has)
+{
+	struct litest_device *dev = litest_current_device();
+
+	ck_assert_int_eq(libinput_device_switch_has_switch(dev->libinput_device,
+							   LIBINPUT_SWITCH_LID),
+			 1);
+}
+END_TEST
+
 START_TEST(switch_toggle)
 {
 	struct litest_device *dev = litest_current_device();
@@ -605,6 +615,7 @@ void
 litest_setup_tests_lid(void)
 {
 	litest_add("switch:has", switch_has_cap, LITEST_SWITCH, LITEST_ANY);
+	litest_add("switch:has", switch_has, LITEST_SWITCH, LITEST_ANY);
 	litest_add("switch:toggle", switch_toggle, LITEST_SWITCH, LITEST_ANY);
 	litest_add("switch:toggle", switch_toggle_double, LITEST_SWITCH, LITEST_ANY);
 	litest_add("switch:toggle", switch_down_on_init, LITEST_SWITCH, LITEST_ANY);