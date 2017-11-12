87920f49926581ee5b3a2735edf41b007c74b29d
Peter Hutterer <peter.hutterer@who-t.net>
2017-11-13 09:30:30 +1000

    fallback: create the evdev-fallback.h header file
    So we can split up evdev-fallback.c into multiple files where needed.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/evdev-fallback.c
+++ src/evdev-fallback.c
@@ -28,100 +28,10 @@
 
 #include <mtdev-plumbing.h>
 
-#include "evdev.h"
+#include "evdev-fallback.h"
 
 #define	DEBOUNCE_TIME ms2us(12)
 
-struct fallback_dispatch {
-	struct evdev_dispatch base;
-	struct evdev_device *device;
-
-	struct libinput_device_config_calibration calibration;
-
-	struct {
-		bool is_enabled;
-		int angle;
-		struct matrix matrix;
-		struct libinput_device_config_rotation config;
-	} rotation;
-
-	struct {
-		struct device_coords point;
-		int32_t seat_slot;
-
-		struct {
-			struct device_coords min, max;
-			struct ratelimit range_warn_limit;
-		} warning_range;
-	} abs;
-
-	struct {
-		int slot;
-		struct mt_slot *slots;
-		size_t slots_len;
-		bool want_hysteresis;
-		struct device_coords hysteresis_margin;
-	} mt;
-
-	struct device_coords rel;
-
-	struct {
-		/* The struct for the tablet mode switch device itself */
-		struct {
-			int state;
-		} sw;
-		/* The struct for other devices listening to the tablet mode
-		   switch */
-		struct {
-			struct evdev_device *sw_device;
-			struct libinput_event_listener listener;
-		} other;
-	} tablet_mode;
-
-	/* Bitmask of pressed keys used to ignore initial release events from
-	 * the kernel. */
-	unsigned long hw_key_mask[NLONGS(KEY_CNT)];
-
-	enum evdev_event_type pending_event;
-
-	/* true if we're reading events (i.e. not suspended) but we're
-	   ignoring them */
-	bool ignore_events;
-
-	struct {
-		enum evdev_debounce_state state;
-		unsigned int button_code;
-		uint64_t button_up_time;
-		struct libinput_timer timer;
-	} debounce;
-
-	struct {
-		enum switch_reliability reliability;
-
-		bool is_closed;
-		bool is_closed_client_state;
-
-		/* We allow up to 3 paired keyboards for the lid switch
-		 * listener. Only one keyboard should exist, but that can
-		 * have more than one event node.
-		 *
-		 * Note: this is a sparse list, any element may have a
-		 * non-NULL device.
-		 */
-		struct paired_keyboard {
-			struct evdev_device *device;
-			struct libinput_event_listener listener;
-		} paired_keyboard[3];
-	} lid;
-};
-
-static inline struct fallback_dispatch*
-fallback_dispatch(struct evdev_dispatch *dispatch)
-{
-	evdev_verify_dispatch_type(dispatch, DISPATCH_FALLBACK);
-
-	return container_of(dispatch, struct fallback_dispatch, base);
-}
 enum key_type {
 	KEY_TYPE_NONE,
 	KEY_TYPE_KEY,
