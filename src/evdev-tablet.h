4ebb131bc7e411f34130d57b7efa7a5b62b0fdb8
Lyude Paul <thatslyude@gmail.com>
2017-11-17 08:40:07 +1000

    Correct Lyude's Copyright assignment
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/evdev-tablet.h
+++ src/evdev-tablet.h
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2014 Red Hat, Inc.
- * Copyright © 2014 Stephen Chandler "Lyude" Paul
+ * Copyright © 2014 Lyude Paul
  *
  * Permission to use, copy, modify, distribute, and sell this software and
  * its documentation for any purpose is hereby granted without fee, provided
