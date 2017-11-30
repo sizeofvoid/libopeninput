0843fa8e5e477a926eb7536c47c5421f8ddf8759
Quentin Glidic <sardemff7+git@sardemff7.net>
2017-11-30 09:23:39 +0100

    meson: Fix bindir usage in install script
    Since the install script cannot know the correct bindir, just pass it
    from Meson directly.
    
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- tools/install-compat-scripts.sh
+++ tools/install-compat-scripts.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-#
-# This does not honor $bindir properly, because we cannot get to it
-# here. Does anyone build to something but prefix/bin?
-#
-bindir="${DESTDIR}/${MESON_INSTALL_PREFIX}/bin"
-mkdir -p "$bindir"
+
+bindir="${DESTDIR}${1}"
+
+# Do not create bindir, because if it is not there now, we have a problem
 cp "${MESON_SOURCE_ROOT}/tools/libinput-list-devices.compat" "${bindir}/libinput-list-devices"
 cp "${MESON_SOURCE_ROOT}/tools/libinput-debug-events.compat" "${bindir}/libinput-debug-events"
