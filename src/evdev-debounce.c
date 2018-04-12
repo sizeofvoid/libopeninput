23614f75515ddb97089e2554a304aac3d951cff3
Peter Hutterer <peter.hutterer@who-t.net>
2018-04-13 08:57:05 +1000

    debounce: disable debouncing on the Logitech K400
    This is an external keyboard+touchpad but not recognised as touchpad by the
    kernel so it's in mouse emulation mode. Double-taps are sent with impossibly
    close timestamps and filtered out by the debouncing code. Since this isn't a
    real button that can wear out anyway, let's just disable debouncing on this
    device.
    
    https://bugs.freedesktop.org/show_bug.cgi?id=105974
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/evdev-debounce.c
+++ src/evdev-debounce.c
@@ -568,7 +568,8 @@ fallback_init_debounce(struct fallback_dispatch *dispatch)
 	struct evdev_device *device = dispatch->device;
 	char timer_name[64];
 
-	if (device->model_flags & EVDEV_MODEL_MS_NANO_TRANSCEIVER) {
+	if (device->model_flags &
+	    (EVDEV_MODEL_MS_NANO_TRANSCEIVER|EVDEV_MODEL_LOGITECH_K400)) {
 		dispatch->debounce.state = DEBOUNCE_STATE_DISABLED;
 		return;
 	}
