8a223e739b0a35e286675f945a890b2738022332
Peter Hutterer <peter.hutterer@who-t.net>
2018-04-13 14:45:19 +1000

    Drop the libinput-list-devices and libinput-debug-events compat scripts
    Added in 1.8, let's drop them for 1.11
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- tools/install-compat-scripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-bindir="${DESTDIR}${1}"
-
-# Do not create bindir, because if it is not there now, we have a problem
-cp "${MESON_SOURCE_ROOT}/tools/libinput-list-devices.compat" "${bindir}/libinput-list-devices"
-cp "${MESON_SOURCE_ROOT}/tools/libinput-debug-events.compat" "${bindir}/libinput-debug-events"
