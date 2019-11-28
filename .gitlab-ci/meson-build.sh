142399f8a39c2f679286fae93ee03be8d7c75e34
Peter Hutterer <peter.hutterer@who-t.net>
2019-11-28 11:35:54 +1000

    gitlab CI: move the build instructions into a bash file
    Extending/debugging scripts in the gitlab CI directly is a pain, the
    turnaround cycle is terrible. Let's move this into a shellscript that we can
    just call directly.
    
    Bonus side-effect: if we wanted to extend the script: set somewhere, this is
    now much easier to override.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

---
+++ .gitlab-ci/meson-build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [[ -z "$MESON_BUILDDIR" ]]; then
+	echo "\$MESON_BUILDDIR undefined."
+	exit 1
+fi
+
+# emulate a few gitlab variables to make it easier to
+# run and debug locally.
+if [[ -z "$CI_JOB_ID" ]] || [[ -z "$CI_JOB_NAME" ]]; then
+	echo "Missing \$CI_JOB_ID or \$CI_JOB_NAME".
+	CI_JOB_ID=$(date +%s)
+	CI_JOB_NAME='libinput-job-local'
+	echo "Simulating gitlab environment: "
+	echo " CI_JOB_ID=$CI_JOB_ID"
+	echo " CI_JOB_NAME=$CI_JOB_NAME"
+fi
+
+
+echo "*************************************************"
+echo "builddir: $MESON_BUILDDIR"
+echo "meson args: $MESON_ARGS"
+echo "ninja args: $NINJA_ARGS"
+echo "meson test args: $MESON_TEST_ARGS"
+echo "*************************************************"
+
+set -e
+
+rm -rf "$MESON_BUILDDIR"
+meson "$MESON_BUILDDIR" $MESON_ARGS
+meson configure "$MESON_BUILDDIR"
+ninja -C "$MESON_BUILDDIR" $NINJA_ARGS
+
+if [[ -z "$MESON_TEST_ARGS" ]]; then
+    exit 0
+fi
+
+# we still want to generate the reports, even if meson test fails
+set +e
+meson test -C "$MESON_BUILDDIR" $MESON_TEST_ARGS
+exit_code=$?
+set -e
+
+# We need the glob for the testlog so that it picks up those suffixed by a
+# suite (e.g. testlog-valgrind.json)
+./.gitlab-ci/meson-junit-report.py \
+	--project-name=libinput \
+	--job-id="$CI_JOB_ID" \
+	--output="$MESON_BUILDDIR/junit-$CI_JOB_NAME-report.xml" \
+	"$MESON_BUILDDIR"/meson-logs/testlog*.json; \
+
+exit $exit_code
