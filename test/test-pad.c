4f6949cf2cbef059f360adb775a8b4be292a19bd
José Expósito <jose.exposito89@gmail.com>
2022-11-23 18:28:01 +0100

    test: remove unused variable
    Fix warning building with Clang v15:
    
    	../test/test-pad.c:334:15: warning: variable 'expected_number'
    	set but not used [-Wunused-but-set-variable]
    		unsigned int expected_number = 0;
    
    Signed-off-by: José Expósito <jose.exposito89@gmail.com>

--- test/test-pad.c
+++ test/test-pad.c
@@ -331,7 +331,6 @@ START_TEST(pad_button_mode_groups)
 	struct litest_device *dev = litest_current_device();
 	struct libinput *li = dev->libinput;
 	unsigned int code;
-	unsigned int expected_number = 0;
 	struct libinput_event *ev;
 	struct libinput_event_tablet_pad *pev;
 
@@ -381,8 +380,6 @@ START_TEST(pad_button_mode_groups)
 		index = libinput_tablet_pad_mode_group_get_index(group);
 		ck_assert_int_eq(index, 0);
 		libinput_event_destroy(ev);
-
-		expected_number++;
 	}
 
 	litest_assert_empty_queue(li);
