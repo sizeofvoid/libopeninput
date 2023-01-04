b044b5fae135ea6a37d94e5d942b015db09b7f9a
Peter Hutterer <peter.hutterer@who-t.net>
2023-01-04 16:17:23 +1000

    gitlab CI: explicitly run the test in the systemd service
    This shouldn't have functional changes because MESON_TEST_ARGS was set
    for all our b2c/qemu tests and thus the tests would run automatically.
    To make this script more generic, specify --run-test explicitly.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- .gitlab-ci/start-in-systemd.sh
+++ .gitlab-ci/start-in-systemd.sh
@@ -37,7 +37,7 @@ Type=simple
 StandardOutput=journal+console
 EnvironmentFile=$B2C_WORKDIR/.b2c_env
 WorkingDirectory=$WORKDIR
-ExecStart=$WORKDIR/.gitlab-ci/meson-build.sh --skip-setup --skip-build
+ExecStart=$WORKDIR/.gitlab-ci/meson-build.sh --skip-setup --skip-build --run-test
 
 # exit the container on termination
 ExecStopPost=$post_command
