f5cc45e409e393f620aa7aa292dac07a968ddf37
Peter Hutterer <peter.hutterer@who-t.net>
2024-10-22 11:54:45 +1000

    tools: add rel dial support to libinput-list-devices
    Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1068>

--- tools/libinput-list-devices.c
+++ tools/libinput-list-devices.c
@@ -259,12 +259,13 @@ rotation_default(struct libinput_device *device)
 static void
 print_pad_info(struct libinput_device *device)
 {
-	int nbuttons, nrings, nstrips, ngroups, nmodes;
+	int nbuttons, nrings, nstrips, ndials, ngroups, nmodes;
 	struct libinput_tablet_pad_mode_group *group;
 
 	nbuttons = libinput_device_tablet_pad_get_num_buttons(device);
 	nrings = libinput_device_tablet_pad_get_num_rings(device);
 	nstrips = libinput_device_tablet_pad_get_num_strips(device);
+	ndials = libinput_device_tablet_pad_get_num_dials(device);
 	ngroups = libinput_device_tablet_pad_get_num_mode_groups(device);
 
 	group = libinput_device_tablet_pad_get_mode_group(device, 0);
@@ -273,6 +274,7 @@ print_pad_info(struct libinput_device *device)
 	printf("Pad:\n");
 	printf("	Rings:   %d\n", nrings);
 	printf("	Strips:  %d\n", nstrips);
+	printf("	Dials:   %d\n", ndials);
 	printf("	Buttons: %d\n", nbuttons);
 	printf("	Mode groups: %d (%d modes)\n", ngroups, nmodes);
 
