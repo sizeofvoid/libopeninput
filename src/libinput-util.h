5842915d213b5b2b728d04880e1cb565e66fe2b9
Peter Hutterer <peter.hutterer@who-t.net>
2024-12-20 15:41:47 +1000

    util: add etrace to trace to stderr
    Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1116>

--- src/libinput-util.h
+++ src/libinput-util.h
@@ -57,12 +57,15 @@
 #define DEFAULT_MOUSE_DPI 1000
 #define DEFAULT_TRACKPOINT_SENSITIVITY 128
 
-#define trace(...) \
+#define _trace(file, ...) \
 	do { \
 	char buf_[1024]; \
 	snprintf(buf_, sizeof(buf_), __VA_ARGS__); \
-	printf(ANSI_BLUE "%s():%d - " ANSI_RED "%s" ANSI_NORMAL "\n", __func__, __LINE__, buf_); \
+	fprintf(file, ANSI_BLUE "%s():%d - " ANSI_RED "%s" ANSI_NORMAL "\n", __func__, __LINE__, buf_); \
 	} while (0)
+
+#define trace(...) _trace(stdout, __VA_ARGS__)
+#define etrace(...) _trace(stderr, __VA_ARGS__)
 
 #define LIBINPUT_EXPORT __attribute__ ((visibility("default")))
 #define LIBINPUT_UNUSED __attribute__ ((unused))
