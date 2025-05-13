842a7ed93e15519941f809d5959e1d8bcdb2c980
Peter Hutterer <peter.hutterer@who-t.net>
2025-05-13 16:21:16 +1000

    Fix links to point to the current doc pages
    The underscored page names date back to doxygen and have been obsolete
    for many years now.
    
    Closes #1123
    
    Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1202>

--- src/evdev.h
+++ src/evdev.h
@@ -1025,7 +1025,7 @@ evdev_device_check_abs_axis_range(struct evdev_device *device,
 		log_info_ratelimit(evdev_libinput_context(device),
 				   &device->abs.warning_range.range_warn_limit,
 				   "Axis %#x value %d is outside expected range [%d, %d]\n"
-				   "See %s/absolute_coordinate_ranges.html for details\n",
+				   "See %s/absolute-coordinate-ranges.html for details\n",
 				   code, value, min, max,
 				   HTTP_DOC_LINK);
 	}
