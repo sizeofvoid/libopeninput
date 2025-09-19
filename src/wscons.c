5e93d5c82d455c3901c2cb9b40dbbe5358a88e30
kikadf <kikadf.01@gmail.com>
2025-09-19 16:23:14 +0200

    Use portable wsevent>type checks instead of OS-specific macros

--- src/wscons.c
+++ src/wscons.c
@@ -209,15 +209,17 @@ wscons_process(struct libinput_device *device, struct wscons_event *wsevent)
 		axis_notify_event(device, time, &accel, &raw);
 		break;
 
-#ifndef __NetBSD__
+#ifdef WSCONS_EVENT_SYNC
 	case WSCONS_EVENT_SYNC:
 		break;
 #endif
 
 	case WSCONS_EVENT_MOUSE_ABSOLUTE_Z:
 	case WSCONS_EVENT_MOUSE_ABSOLUTE_W:
-#ifndef __NetBSD__
+#ifdef WSCONS_EVENT_TOUCH_WIDTH
 	case WSCONS_EVENT_TOUCH_WIDTH:
+#endif
+#ifdef WSCONS_EVENT_TOUCH_RESET
 	case WSCONS_EVENT_TOUCH_RESET:
 #endif
 		/* ignore those */
