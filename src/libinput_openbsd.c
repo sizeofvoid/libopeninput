9a06bb157891cb34d25df51c20f5114379007546
GH <47375452+VlkrS@users.noreply.github.com>
2026-03-06 23:12:28 +0100

    Implement v120 Wheel API

--- src/libinput_openbsd.c
+++ src/libinput_openbsd.c
@@ -2414,15 +2414,19 @@ axis_notify_event(struct libinput_device *device,
     const struct device_float_coords *raw)
 {
 	struct libinput_event_pointer *axis_event;
+	struct wheel_v120 v120 = { 0.0, 0.0 };
 	uint32_t axes;
 
 	axis_event = zalloc(sizeof *axis_event);
 	if (!axis_event)
 		return;
-	if (delta->x)
+	if (delta->x) {
 		axes = bit(LIBINPUT_POINTER_AXIS_SCROLL_HORIZONTAL);
-	else
+		v120.x = delta->x * 3.75;
+	} else {
 		axes = bit(LIBINPUT_POINTER_AXIS_SCROLL_VERTICAL);
+		v120.x = delta->y * 3.75;
+	}
 
 	*axis_event = (struct libinput_event_pointer) {
 		.time = time,
@@ -2430,6 +2434,9 @@ axis_notify_event(struct libinput_device *device,
 		.delta_raw = *raw,
 		.source = LIBINPUT_POINTER_AXIS_SOURCE_WHEEL,
 		.axes = axes,
+		.discrete.x = 0,
+		.discrete.y = 0,
+		.v120 = v120
 	};
 
 	post_device_event(device, time, LIBINPUT_EVENT_POINTER_SCROLL_WHEEL,
