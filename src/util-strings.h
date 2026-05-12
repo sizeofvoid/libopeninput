mix-mirror/libinput

commit 75d4acfe94d012a3ddab6bfc4a4cb9cb5fee7f2d

util: allow for 'e' in safe_atod strings
1.5e2 is a valid string, so let's parse those.

Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1467>

src/util-strings.h
@@ -282,6 +282,7 @@ safe_atod(const char *str, double *val)
 		case '+':
 		case '-':
 		case '.':
+		case 'e':
 			break;
 		default:
 			return false;

test/test-utils.c
@@ -1612,6 +1612,7 @@ START_TEST(safe_atod_test)
 		{ "2147483647", true, 2147483647 },
 		{ "-2147483648", true, -2147483648 },
 		{ "4294967295", true, 4294967295 },
+		{ "-1.3e-1", true, -0.130},
 		{ "0x0", false, 0 },
 		{ "0x10", false, 0 },
 		{ "0xaf", false, 0 },

sizeofvoid/libopeninput

commit 61b3b785ae1232aabaf6da81c60c327580e988f2

util: allow for uppercase 'E' in safe_atod strings too
Fixes: 75d4acfe94d0 ("util: allow for 'e' in safe_atod strings")
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1475>

src/util-strings.h
@@ -283,6 +283,7 @@ safe_atod(const char *str, double *val)
 		case '-':
 		case '.':
 		case 'e':
+		case 'E':
 			break;
 		default:
 			return false;

test/test-utils.c
@@ -1613,6 +1613,7 @@ START_TEST(safe_atod_test)
 		{ "-2147483648", true, -2147483648 },
 		{ "4294967295", true, 4294967295 },
 		{ "-1.3e-1", true, -0.130},
+		{ "1.5E2", true, 150.0},
 		{ "0x0", false, 0 },
 		{ "0x10", false, 0 },
 		{ "0xaf", false, 0 },